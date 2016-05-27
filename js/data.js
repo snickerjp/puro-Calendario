a8f3dafa906ab6e22a2da0789a474df7bd27aac4
snickerjp <snickerjp@gmail.com>
2016-05-27 16:55:29 +0900

    add files

---
+++ js/data.js
@@ -0,0 +1,9 @@
+var codropsEvents = {
+	'05-14-2016' : '<a href="http://puro-fan.hatenablog.com/entry/2016-05-14-parade-dancer-index">2016年5月14日の『Miracle Gift Parade(ミラクルギフトパレード)』出演ダンサー配役一覧</a>',
+	'05-21-2016' : '<a href="http://puro-fan.hatenablog.com/entry/2016-05-21-parade-dancer-index">2016年5月21日13時の『Miracle Gift Parade(ミラクルギフトパレード)』出演ダンサー配役一覧</a>',
+	'05-22-2016' : '<a href="http://puro-fan.hatenablog.com/entry/2016-05-22-parade-dancer-index">2016年5月22日13時の『Miracle Gift Parade(ミラクルギフトパレード)』出演ダンサー配役一覧</a>',
+	'05-23-2016' : '<a href="http://puro-fan.hatenablog.com/entry/2016-05-23-parade-dancer-index">2016年5月23日の『Miracle Gift Parade(ミラクルギフトパレード)』出演ダンサー配役一覧</a>',
+	'05-24-2016' : '<a href="http://puro-fan.hatenablog.com/entry/2016-05-24-parade-dancer-index">2016年5月24日の『Miracle Gift Parade(ミラクルギフトパレード)』出演ダンサー配役一覧</a>',
+	'05-25-2016' : '<a href="http://puro-fan.hatenablog.com/entry/2016-05-25-parade-dancer-index">2016年5月25日の『Miracle Gift Parade(ミラクルギフトパレード)』出演ダンサー配役一覧</a>',
+	'05-26-2016' : '<a href="http://puro-fan.hatenablog.com/entry/2016-05-26-parade-dancer-index">2016年5月26日の『Miracle Gift Parade(ミラクルギフトパレード)』出演ダンサー配役一覧</a>',
+};
